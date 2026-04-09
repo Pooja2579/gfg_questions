591f8c32c922c2fe27f6679c7a63896921053cb6
Pooja <103728019+Pooja2579@users.noreply.github.com>
2026-04-09 23:46:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Missing in Array/missing-in-array.py
+++ Difficulty: Easy/Missing in Array/missing-in-array.py
@@ -1,7 +1,9 @@
 class Solution:
     def missingNum(self, arr):
         # code here
-        n = len(arr) + 1
-        total_sum = n*(n+1) // 2
-        sum_arr = sum(arr)
-        return total_sum - sum_arr
+        n = len(arr) + 1   # because one number is missing
+
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(arr)
+    
+        return expected_sum - actual_sum
